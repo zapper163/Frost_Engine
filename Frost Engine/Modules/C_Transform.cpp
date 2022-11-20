8248037218d5e315353c3874a246e05211449d5f
zapper163 <zapingero163@gmail.com>
2022-11-20 11:50:08 +0100

    set transform go parent is null

--- Frost Engine/Modules/C_Transform.cpp
+++ Frost Engine/Modules/C_Transform.cpp
@@ -39,10 +39,10 @@ void C_Transform::SetTransform(float3 position, Quat rotation, float3 scale)
 
 	transform.localPos = float4x4::FromTRS(transform.position, transform.quatRotation, transform.scale);
 
-	if (go->parent->transform != nullptr) {
+	/*if (go->parent->transform != nullptr) {
 		transform.globalPos = go->parent->transform->transform.globalPos * transform.localPos;
 	}
-	transform.transGlobalPos = transform.globalPos.Transposed();
+	transform.transGlobalPos = transform.globalPos.Transposed();*/
 }
 
 void C_Transform::OnGui()
